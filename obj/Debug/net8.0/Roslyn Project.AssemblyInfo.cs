29085009275c2f9fc138c312901b3cbccd923ee9
astha <astha.dwivedi@prolifics.com>
2025-04-02 08:52:52 +0530

    Adding R

--- obj/Debug/net8.0/Roslyn Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Roslyn Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Roslyn Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+667b2b38e882bd4feb5864ef2bf5bb796cd46596")]
 [assembly: System.Reflection.AssemblyProductAttribute("Roslyn Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Roslyn Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
